01a26824847212018d61f7e9b24aa93480a4b248
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-07-31 01:30:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function mapLetters(word) {
+    const result = {};
+
+    for (let i = 0; i < word.length; i++) {
+        const char = Symbol.for(word[i]);
+        if (!result[char]) {
+            result[char] = [];
+        }
+        result[char].push(i);
+    }
+
+    return result;
+}
